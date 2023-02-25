a9cae72c65dab53353111c09e08bf85c76719150
For x in y <114295418+forxiny@users.noreply.github.com>
2023-02-25 09:19:20 -0500

    initial commit for pushshift_data.py

---
+++ src/pushshift_data.py
@@ -0,0 +1,16 @@
+"""
+Utility functions for processing Reddit data from Pushshift API and monthly archives.
+"""
+
+from pathlib import Path
+import polars as pl
+
+def get_archive_file_paths(folder):
+    """
+    Get file paths for all json files in the specified folder
+
+    :param folder: path to folder holding decompressed pushshift monthly files
+    :return: generator of file paths
+    """
+
+    pass
