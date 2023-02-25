adfa60439499c50d30af14ba81b52353c7cfb9e1
For x in y <114295418+forxiny@users.noreply.github.com>
2023-02-25 09:30:18 -0500

    add docstring

--- tests/test_pushshift_data.py
+++ tests/test_pushshift_data.py
@@ -5,5 +5,8 @@ Tests related to the pushshift_data.py module
 import src.pushshift_data as psh_data
 
 def test_get_archive_filepaths():
-
+    """
+    check if get_archive_filepaths method exists
+    :return: bool
+    """
     assert "get_archive_filepaths" in dir(psh_data)
