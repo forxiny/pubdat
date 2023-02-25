57a2590cd2f6ebd67783f2be6cf8b7ef2174e165
For x in y <114295418+forxiny@users.noreply.github.com>
2023-02-25 09:27:28 -0500

    add first test for pushshift data

--- tests/test_pushshift_data.py
+++ tests/test_pushshift_data.py
@@ -2,6 +2,8 @@
 Tests related to the pushshift_data.py module
 """
 
+import src.pushshift_data as psh_data
 
 def test_get_archive_filepaths():
-    pass
+
+    assert "get_archive_filepaths" in dir(psh_data)
